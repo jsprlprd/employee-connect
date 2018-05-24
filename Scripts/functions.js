787d44746e8d7c0ced0509cfb3968617786d51d0
JSPR Design <jspr.dsgn@gmail.com>
2018-05-24 15:11:20 +0800

    Mobile Navigation

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -16,3 +16,55 @@ $('.collapsible').on('hidden.bs.collapse',function(){
   activeMenu.parent().addClass('active');
   inactiveMenus.parent().removeClass('active');
 });
+
+
+
+$(function(){
+  $('.navbar-toggle').click(function(){
+    if($("#LEFTPANEL").hasClass('close-pane')){
+      $( "#LEFTPANEL" ).animate({
+        left: "+=300"
+      }, 350, function() {
+            // Animation complete.
+      });
+      $("#LEFTPANEL").removeClass('close-pane').addClass('open-pane');
+    } else {   	
+      $( "#LEFTPANEL" ).animate({
+        left: "-=300"
+      }, 350, function() {
+            // Animation complete.
+      });
+      $("#LEFTPANEL").removeClass('open-pane').addClass('close-pane');
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
